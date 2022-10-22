77c7314d8b1382018946dba1bc3d93810f34c517
Sean <seandesign01@gmail.com>
2022-10-22 11:08:47 +0800

    update

--- inc/js/base.js
+++ inc/js/base.js
@@ -2,3 +2,19 @@ $(window).ready(function() {
     $(".page-header").load("html/page-header.html");
     $(".page-footer").load("html/page-footer.html");
 });
+
+
+$(function() {
+	$(window).scroll(function() {
+	  if ($(this).scrollTop() > 240) {
+		$(".btn-gotop").fadeIn();
+	  } else {
+		$(".btn-gotop").fadeOut();
+	  }
+	});
+	$(".btn-gotop").click(function() {
+	  $('body, html').animate({
+		scrollTop: 0
+	  }, 500);
+	});
+});
